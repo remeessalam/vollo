7d017631df75f23414333253487213a59d9c3d70
remeessalam <remeesreme4u@gmail.com>
2024-12-03 18:36:47 +0530

    home banner also added bottom add animation

--- src/pages/HomePageSections/Banner.jsx
+++ src/pages/HomePageSections/Banner.jsx
@@ -48,10 +48,18 @@ const Banner = () => {
       <div className="relative bottom-0 max-w-[90rem]  lg:translate-y-1/2 lg:mt-0 mt-10 border lg:border-none border-gray-300 lg:absolute shadow-lg rounded-lg w-[90%] sm:w-[80%] h-full  lg:h-[330px]  2xl:h-[270px] bg-white grid grid-cols-1 lg:grid-cols-3">
         <div className="w-full h-full border-dashed  border-b lg:border-b-0 flex flex-col p-5 justify-center gap-4 hover:bg-buttonBackground  items-center">
           <div className="w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-lg">
-            <img src={AdvancedAISolutions} alt="" width={40} height={40} />
+            <img
+              data-aos="fade-up"
+              src={AdvancedAISolutions}
+              alt=""
+              width={40}
+              height={40}
+            />
           </div>
-          <h4 className="text-head text-center">Advanced AI Solutions</h4>
-          <p className="text-center text-desc">
+          <h4 data-aos="fade-up" className="text-head text-center">
+            Advanced AI Solutions
+          </h4>
+          <p data-aos="fade-up" className="text-center text-desc">
             Harnessing the power of AI, we craft innovative tools to automate
             processes, enhance decision-making, and unlock growth opportunities
             tailored to your business needs.
@@ -60,16 +68,17 @@ const Banner = () => {
         <div className="border-dashed  lg:border-x w-ful h-[270px]l flex flex-col md:p-2 lg:p-5  p-5 items-center justify-center gap-4 hover:bg-buttonBackground">
           <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-lg">
             <img
+              data-aos="fade-up"
               src={ScalableCloudInfrastructure}
               alt=""
               width={40}
               height={40}
             />
           </div>
-          <h4 className="text-head text-center">
+          <h4 data-aos="fade-up" className="text-head text-center">
             Scalable Cloud Infrastructure
           </h4>
-          <p className="text-center text-desc">
+          <p data-aos="fade-up" className="text-center text-desc">
             Our cutting-edge cloud solutions ensure seamless operations,
             offering flexibility, security, and scalability for businesses of
             all sizes to thrive in a dynamic digital landscape.
@@ -77,10 +86,18 @@ const Banner = () => {
         </div>
         <div className="w-full h-full border-dashed  border-t lg:border-t-0 flex flex-col p-5 items-center justify-center gap-4 hover:bg-buttonBackground">
           <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-lg">
-            <img src={DataDrivenInsights} alt="" width={40} height={40} />
+            <img
+              data-aos="fade-up"
+              src={DataDrivenInsights}
+              alt=""
+              width={40}
+              height={40}
+            />
           </div>
-          <h4 className="text-head">Data-Driven Insights</h4>
-          <p className="text-center text-desc">
+          <h4 data-aos="fade-up" className="text-head">
+            Data-Driven Insights
+          </h4>
+          <p data-aos="fade-up" className="text-center text-desc">
             Transform raw data into actionable strategies with our advanced
             analytics solutions, empowering businesses to make informed
             decisions and stay ahead in the competitive market.
